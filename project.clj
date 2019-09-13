7e3516f250dfac6eab4bc8c42daa7d574c6eb106
Rick Moynihan <rick.m@swirrl.com>
2019-09-13 13:23:24 +0100

    Tidy up project.clj

--- project.clj
+++ project.clj
@@ -10,7 +10,7 @@
 
   :profiles {:provided {
 
-                        :dependencies [[org.clojure/clojure "1.10.0"]
+                        :dependencies [[org.clojure/clojure "1.10.1"]
                                        [org.clojure/core.async "0.4.490"]
 
 
@@ -23,15 +23,10 @@
 
                                        ]
 
-                        :resource-paths ["vendor/REBL-0.9.149.jar"]}
+                        :resource-paths ["vendor/REBL-0.9.220.jar"]}
 
 
-             :repl {:repl-options {:nrepl-middleware [nrebl.middleware/wrap-nrebl]}}
-
-             ;;:dev {:dependencies [[nrepl "0.6.0"]]} ;; Used by lein 2.8.3+ if you're on leiningen 2.9.0+ you can omit this
-
-             :dev-old {:dependencies [[org.clojure/tools.nrepl "0.2.13"]]} ;; lein < 2.8.3
-             }
+             :repl {:repl-options {:nrepl-middleware [nrebl.middleware/wrap-nrebl]}}}
 
   :min-lein-version "2.9.0" ;; nrepl 0.6.0
   )
